6b7ac47032da734dfd277b0fce5e563350b69107
Sean <seanbarber@outlook.com>
2018-07-10 12:55:03 -0500

    Cleaning up code and adding in documentation

--- app/src/main/java/daslab/com/fieldmonitoringv2/Plan.java
+++ app/src/main/java/daslab/com/fieldmonitoringv2/Plan.java
@@ -1,19 +1,14 @@
 package daslab.com.fieldmonitoringv2;
 
-import android.util.Log;
-
-import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Plan{
-    int numberOfWaypoints;
+    private int numberOfWaypoints;
     String file;
     String planName;
     List<Marker> markerLinkedList = new LinkedList<>();
@@ -24,20 +19,8 @@ public class Plan{
         this.planName = planName;
     }
 
-    public int getNumberOfWaypoints() {
-        return numberOfWaypoints;
-    }
-
-    public String getPlanName() {
-        return planName;
-    }
-
     public String getFile() {
         return file;
-    }
-
-    public List<Marker> getMarkerLinkedList() {
-        return markerLinkedList;
     }
 
     public void writeToPlan(FileWriter fileWriter){
@@ -54,24 +37,6 @@ public class Plan{
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public List<LatLng> readFromPlan(){
-        List<LatLng> latLngList = new LinkedList<>();
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(file));
-            String line, line1;
-            while ((line = br.readLine()) != null &&
-                    (line1 = br.readLine()) != null) {
-                Log.d("line", line + " & " + line1);
-                latLngList.add(new LatLng(Double.parseDouble(line), Double.parseDouble(line1)));
-            }
-            br.close();
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-        return latLngList;
     }
 
 }
