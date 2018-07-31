4ae556f396677d0c00b0dd2cd9d7b8a0ee17acfc
Sean <seanbarber@outlook.com>
2018-07-31 17:16:01 -0500

    Testing one rgb value

--- app/src/main/java/daslab/com/fieldmonitoringv2/AnomalyDetection.java
+++ app/src/main/java/daslab/com/fieldmonitoringv2/AnomalyDetection.java
@@ -2,7 +2,10 @@ package daslab.com.fieldmonitoringv2;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.util.Log;
+
+import java.util.ArrayList;
 
 public class AnomalyDetection {
     String imagePath;
@@ -27,6 +30,15 @@ public class AnomalyDetection {
         Log.d("imageHeight", String.valueOf(imageHeight));
         this.imageWidth = this.image.getWidth();
         Log.d("imageWidth", String.valueOf(imageWidth));
+        int color = this.image.getPixel(0,0);
+        int red = Color.red(color);
+        int blue = Color.blue(color);
+        int green = Color.green(color);
+        int alpha = Color.alpha(color);
+        float[] hsv = new float[3];
+        Color.colorToHSV(color,hsv);
+        Log.d("hsv", String.valueOf(hsv[0]) + "," + String.valueOf(hsv[1]) + "," + String.valueOf(hsv[2]));
+        Log.d("color", red + ", " + green + ", " + blue + ", " + alpha);
     }
 
 }
